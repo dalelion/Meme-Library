ad4f6be1a14e6af329bec072451cfe9e4606af37
John Hollett <jh2587@mun.ca>
2020-06-22 01:07:25 -0230

    Import Fixes to get rid of squigles mcgee

--- client/index.tsx
+++ client/index.tsx
@@ -1,4 +1,4 @@
 import {Basic} from "./basic";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 ReactDOM.render(<Basic/>, document.body.querySelector("#mount"));
