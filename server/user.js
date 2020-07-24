ae6539b8f1b5654e5d777c88124fd3e0411eaf12
John Hollett <jh2587@mun.ca>
2020-07-24 19:52:38 -0230

    Registration API untested

--- server/user.js
+++ server/user.js
@@ -3,9 +3,11 @@ const MongoDB = require("./mongo");
 function handleUser(req, res, next) {
 	switch (req.method) {
 		case "GET":
+			res.setHeader("Content-Type", "application/json");
 			res.end();
 			break;
 		case "POST":
+			res.setHeader("Content-Type", "application/json");
 			let body = "";
 			req.on("data", function (data) {
 				body += data;
@@ -15,8 +17,31 @@ function handleUser(req, res, next) {
 				console.log(MESSAGE);
 				
 				MongoDB.Users().then(collection => {
+					collection.findOne({username: MESSAGE.username}).then(row => {
+						if (row) {
+							res.writeHead(400);
+							res.end(JSON.stringify({
+								status: "FAIL",
+								reason: "Username not available"
+							}));
+						} else {
+							if (MESSAGE.password1 === MESSAGE.password2) {
+								collection.insertOne({username: MESSAGE.username, password: MESSAGE.password1}).then(result => {
+									res.writeHead(200);
+									res.end(JSON.stringify({
+										status: "SUCCESS"
+									}));
+								});
+							} else {
+								res.writeHead(400);
+								res.end(JSON.stringify({
+									status: "FAIL",
+									reason: "Password does not match"
+								}));
+							}
+						}
+					})
 				});
-				res.end();
 			});
 			break;
 		default:
